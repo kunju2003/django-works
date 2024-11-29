b6944a2f5e22746b77b7257dd347e92e1fa2329e
kunju2003 <jayalakshmijayan36@gmail.com>
2024-11-29 19:52:33 +0530

    django work

--- django-works/project3/myapp/urls.py
+++ django-works/project3/myapp/urls.py
@@ -1,7 +1,5 @@
 from django.urls import path
 from . import views
-
 urlpatterns=[
-    path('index/<int:data>',views.index)
-
+    path('',views.index)
 ]
--- django-works/project3/myapp/views.py
+++ django-works/project3/myapp/views.py
@@ -2,6 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def index(request,data):
-    print(data)
-    return HttpResponse('welcome'+str(data))
+def index(request):
+    return render(request,'index.html')
